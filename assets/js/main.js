d8a846cc475d8385d28e3ffadc564abe688ba600
emilywschmitt <89418431+emilywschmitt@users.noreply.github.com>
2021-09-13 06:35:17 -0400

    commit 10 - X & Y coordinates

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,35 @@ console.log(bauhaus);
 
 let elements = document.querySelectorAll("div");
 console.log(`This portrait contains ${elements.length} HTML div elements`);
+
+var pointerX = -1;
+var pointerY = -1;
+document.onmousemove = function(event) {
+	pointerX = event.pageX;
+	pointerY = event.pageY;
+}
+setInterval(pointerCheck, 1000);
+function pointerCheck() {
+	console.log('Cursor at: '+pointerX+', '+pointerY);
+}
+
+var myX, myY, xyOn, myMouseX, myMouseY;
+xyOn = true;
+
+function getXYPosition(e) {
+    myMouseX = (e || event).clientX;
+    myMouseY = (e || event).clientY;
+    if (document.documentElement.scrollTop > 0) {
+        myMouseY = myMouseY + document.documentElement.scrollTop;
+    }
+    if (xyOn) {
+        alert("X is " + myMouseX + "\nY is " + myMouseY);
+    }
+}
+function toggleXY() {
+    xyOn = !xyOn;
+    document.getElementById('xyLink').blur();
+    return false;
+}
+
+document.onmouseup = getXYPosition;
